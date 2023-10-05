77f8ad4e39f196b556bb033239c4c4fd4da748b4
Aqueel Ahmed V <aqueelvallanchira13@gmail.com>
2023-10-05 23:01:53 +0530

    Revamp: fix sqlite3 erro?, v1.97

--- server/database.js
+++ server/database.js
@@ -1,8 +1,10 @@
 const { Sequelize } = require("sequelize")
 const fs = require("fs")
+// const sqlite = require("sqlite3")
 
 const db = new Sequelize({
   dialect: 'sqlite',
+  dialectModulePath: "sqlite3",
   storage: './db.sqlite',
 });
 
